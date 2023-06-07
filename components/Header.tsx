c0812b55a69516526e181ffd073668a837971f95
Waleed Arif <waleedarifsiddiqui@gmail.com>
2023-06-07 12:23:05 +0500

    Finished Spotify-clone

---
+++ components/Header.tsx
@@ -0,0 +1,171 @@
+"use client";
+
+import { twMerge } from "tailwind-merge";
+import { useRouter } from "next/navigation";
+import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+import { FaUserAlt } from "react-icons/fa";
+
+import useAuthModal from "@/hooks/useAuthModal";
+import Button from "./Button";
+import { toast } from "react-hot-toast";
+
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useUser } from "@/hooks/useUser";
+
+interface HeaderProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+const Header: React.FC<HeaderProps> = ({ children, className }) => {
+  const authModal = useAuthModal();
+  const router = useRouter();
+
+  const supabaseClient = useSupabaseClient();
+  const { user } = useUser();
+
+  const handleLogout = async () => {
+    const { error } = await supabaseClient.auth.signOut();
+
+    router.refresh();
+
+    if (error) {
+      toast.error(error.message);
+    }
+  };
+  return (
+    <div
+      className={twMerge(
+        `
+            h-fit 
+            bg-gradient-to-b 
+            from-emerald-800 
+            p-6
+        `,
+        className
+      )}
+    >
+      <div className="w-full mb-4 flex items-center justify-between">
+        <div className="hidden md:flex gap-x-2 items-center">
+          <button
+            onClick={() => router.back()}
+            className="
+                        rounded-full 
+                        bg-black 
+                        flex 
+                        items-center 
+                        justify-center 
+                        cursor-pointer 
+                        hover:opacity-75 
+                        transition
+                    "
+          >
+            <RxCaretLeft className="text-white" size={35} />
+          </button>
+          <button
+            onClick={() => router.forward()}
+            className="
+                        rounded-full 
+                        bg-black 
+                        flex 
+                        items-center 
+                        justify-center 
+                        cursor-pointer 
+                        hover:opacity-75 
+                        transition
+                    "
+          >
+            <RxCaretRight className="text-white" size={35} />
+          </button>
+        </div>
+        <div className="flex md:hidden gap-x-2 items-center">
+          <button
+            onClick={() => router.push("/")}
+            className="
+                           rounded-full 
+                           p-2 
+                           bg-white 
+                           flex 
+                           items-center 
+                           justify-center 
+                           cursor-pointer 
+                           hover:opacity-75 
+                           transition
+                    "
+          >
+            <HiHome className="text-black" size={20} />
+          </button>
+          <button
+            onClick={() => router.push("/")}
+            className="
+                           rounded-full 
+                           p-2 
+                           bg-white 
+                           flex 
+                           items-center 
+                           justify-center 
+                           cursor-pointer 
+                           hover:opacity-75 
+                           transition
+                    "
+          >
+            <BiSearch className="text-black" size={20} />
+          </button>
+        </div>
+        <div className="
+            flex
+            justify-between
+            items-center
+            gap-x-4
+        ">
+          {user ? (
+            <div
+              className="flex gap-x-4 items-center">
+              <Button
+                onClick={handleLogout}
+                className="bg-white px-6 py-2"
+              >
+                Logout
+              </Button>
+              <Button
+                onClick={() => router.push('/account')}
+                className="bg-white"
+              >
+                <FaUserAlt />
+              </Button>
+
+            </div>
+          ) : (
+            <>
+              <div>
+                <Button
+                  onClick={authModal.onOpen}
+                  className="
+                       bg-transparent 
+                       text-neutral-300 
+                       font-medium 
+                ">
+                  Sign Up
+                </Button>
+              </div>
+              <div>
+                <Button
+                  onClick={authModal.onOpen}
+                  className="
+                       bg-white px-6 py-2
+                ">
+                  Log In
+                </Button>
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+      {children}
+    </div>
+  );
+};
+
+export default Header;
---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import AuthModal from "@/components/AuthModal";
+// import SubscribeModal from "@/components/SubscribeModal";
+// import UploadModal from "@/components/UploadModal";
+import { ProductWithPrice } from "@/types";
+import UploadModal from "@/components/UploadModal";
+
+interface ModalProviderProps {
+  products: ProductWithPrice[];
+}
+
+const ModalProvider: React.FC<ModalProviderProps> = ({
+  products
+}) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <>
+      <AuthModal />
+      
+      <UploadModal />
+    
+    </>
+  );
+}
+
+export default ModalProvider;
